ecce4b53af8f050588ca044cd7b767ea01980523
aqib <19Nabi@friendsseminary.org>
2018-07-30 11:08:28 -0400

    work

---
+++ renameIt/lib/js/main.js
@@ -0,0 +1,8 @@
+let h1Element = document.getElementById('item_name');
+h1Element.innerHTML = "Frostfang";
+
+
+
+let imgElement = document.querySelector('#item_img')
+let newImgSrc="assets/Frostfang_item.png";
+imgElement.src= newImgSrc;
